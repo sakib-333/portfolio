9cdbfb5625f0efee33170967c0ab5f81aa64fcf3
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-01-30 17:39:19 +0600

    Add navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useState } from "react";
+import Navbar from "./Components/Navbar";
 const App = () => {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: theme,
+    },
+  });
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
   return (
-    <div>
-      <h1 className="text-3xl font-bold bg-red-500">Hello React</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="max-w-screen-2xl mx-auto w-full">
+        <Navbar theme={theme} toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
   );
 };
 
