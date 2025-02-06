3b31461dbc64768bb2581eb40cf14513f760027d
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-02-06 10:57:46 +0600

    Add matrimony portal project.

--- src/assets/myProjects.ts
+++ src/assets/myProjects.ts
@@ -1,6 +1,24 @@
 const myProjects = [
   {
     id: "project-1",
+    projectName: "Matrimony Platform",
+    decription:
+      "A Matrimony Platform is an online platform designed to help individuals find suitable marriage partners. These websites typically offer matchmaking services, where users create profiles, share personal information, and connect with others who are seeking a life partner. Matrimony websites are especially popular in cultures where arranged marriages are common, although they are widely used across the globe.",
+    previewImages: [
+      "https://i.postimg.cc/63gPQDkc/Screenshot-184.png",
+      "https://i.postimg.cc/cC3btFZS/Screenshot-185.png",
+      "https://i.postimg.cc/506GbxRj/Screenshot-186.png",
+      "https://i.postimg.cc/vm8k277r/Screenshot-187.png",
+      "https://i.postimg.cc/MKxDY6mR/Screenshot-188.png",
+    ],
+    frontTech: "React.js, Tailwind, Mamba UI, Axios, and many npm packages.",
+    backTech: "Express, Stripe, MongoDB and npm packages.",
+    liveSite: "https://ph-b10-a12.web.app/",
+    githubRepoFront: "https://github.com/sakib-333/matrimony-platform-client",
+    githubRepoBack: "https://github.com/sakib-333/matrimony-platform-server",
+  },
+  {
+    id: "project-2",
     projectName: "Lost and Found",
     decription:
       "This project is a Lost and Found Website, a platform designed to connect individuals who have lost personal belongings with those who may have found them. Users can report lost items, browse found items, and interact to recover their belongings. Developing this website provides practical experience in building full-stack applications, user authentication, file uploads, database management, and API integration",
@@ -18,7 +36,7 @@ const myProjects = [
     githubRepoBack: "https://github.com/sakib-333/ph-b10-a11-server",
   },
   {
-    id: "project-2",
+    id: "project-3",
     projectName: "Crowdcube: A Crowd Funding",
     decription:
       "A crowdfunding website is a platform where people can raise money for different projects, ideas, or causes by inviting others to contribute financially. These projects can include personal needs (like medical expenses), creative ideas (like making a film or app), and startups (like launching a new product).",
@@ -33,22 +51,6 @@ const myProjects = [
     liveSite: "https://ph-b10-a10.web.app/",
     githubRepoFront: "https://github.com/sakib-333/ph-b10-a10",
     githubRepoBack: "https://github.com/sakib-333/ph-b10-a10-server",
-  },
-  {
-    id: "project-3",
-    projectName: "Winter Clothing Donation",
-    decription:
-      "The Winter Donation web application is a simple and easy-to-use platform that helps people donate winter clothing to those in need across Bangladesh. When the weather gets colder, thousands of vulnerable people, especially in rural and low-income areas, struggle to stay warm because they don’t have enough warm clothing.",
-    previewImages: [
-      "https://i.postimg.cc/Fh0DTLW0/Screenshot-126.png",
-      "https://i.postimg.cc/pdGDnR2J/Screenshot-127.png",
-      "https://i.postimg.cc/wjk5gDZP/Screenshot-128.png",
-    ],
-    frontTech: "React.js, Tailwind, DaisyUI, and many npm packages.",
-    backTech: "N/A",
-    liveSite: "https://sakib-333-ph-b10-a9.netlify.app/",
-    githubRepoFront: "https://github.com/sakib-333/ph-b10-a9",
-    githubRepoBack: "#",
   },
 ];
 
