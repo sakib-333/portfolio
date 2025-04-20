99f9dfab4905eee53ff940132cffb69a52a48fcf
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-04-20 11:31:20 +0600

    Add education section contents.

--- src/Components/Education/Education.tsx
+++ src/Components/Education/Education.tsx
@@ -1,10 +1,41 @@
+import { HiAcademicCap } from "react-icons/hi";
 import useInViewSection from "../../Hooks/useInViewSection";
+import Heading from "../Heading/Heading";
+import educationStack from "./educationStack";
 
 const Education = () => {
   const ref = useInViewSection("education");
   return (
-    <div ref={ref} id="education-section" className="min-h-screen text-white">
-      <h1 className="text-4xl">Education Component</h1>
+    <div ref={ref} id="education-section" className="mt-20 pb-20 px-4 mx-auto">
+      <Heading title="Education" />
+      <p className="max-w-2xl mt-4 mx-auto text-center text-color-4/80">
+        My academic background and educational qualifications that have shaped
+        my skills and knowledge.
+      </p>
+      <ul className="mt-4 timeline timeline-snap-icon max-md:timeline-compact timeline-vertical space-y-3">
+        {educationStack.map((edu, indx) => (
+          <li
+            key={edu.id}
+            className="border-l-3 border-color-2 rounded-md md:border-none"
+          >
+            <div className="timeline-middle hidden md:block text-xl text-color-2 border border-color-2 rounded-full">
+              <HiAcademicCap />
+            </div>
+            <div
+              className={`${
+                indx % 2 ? "timeline-end" : "timeline-start"
+              } md:text-end p-5 w-full rounded-md bg-color-5/50 m-0`}
+            >
+              <div className="text-lg font-black text-color-4">
+                {edu.degree}
+              </div>
+              <p className="text-color-4/80">{edu.institute}</p>
+              <time className="font-mono italic text-color-4">{edu.year}</time>
+            </div>
+            <hr className="bg-gradient-to-b hidden md:block from-color-2 to-color-3" />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
---
+++ src/Components/Education/educationStack.ts
@@ -0,0 +1,22 @@
+const educationStack = [
+  {
+    id: "education-1",
+    degree: "B.Sc(Engg.) in Computer Science & Engineering",
+    institute: "Gopalganj Science and Technology University",
+    year: "2024",
+  },
+  {
+    id: "education-2",
+    degree: "Higher Secondary Certificate(HSC)",
+    institute: "Haji Lalmia City University College, Gopalganj",
+    year: "2017",
+  },
+  {
+    id: "education-3",
+    degree: "Secondary School Certificate(SSC)",
+    institute: "Kotalipara Public Institution",
+    year: "2015",
+  },
+];
+
+export default educationStack;
