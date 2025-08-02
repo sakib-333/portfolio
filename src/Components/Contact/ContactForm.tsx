0a7a5bc62ef4d189226a42b0af19fdb6e02d52fe
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-08-02 09:53:47 +0600

    Update contact form.

--- src/Components/Contact/ContactForm.tsx
+++ src/Components/Contact/ContactForm.tsx
@@ -1,16 +1,46 @@
+import { SyntheticEvent, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { IoIosSend } from "react-icons/io";
 
 const ContactForm = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleSendMail = (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    if (formRef.current) {
+      emailjs
+        .sendForm(
+          import.meta.env.VITE_SERVICE_ID,
+          import.meta.env.VITE_TEMPLATE_ID,
+          formRef.current,
+          {
+            publicKey: import.meta.env.VITE_PUBLIC_KEY,
+          }
+        )
+        .then(
+          () => {
+            console.log("SUCCESS!");
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+    }
+  };
+
   return (
-    <form className="space-y-3">
+    <form ref={formRef} className="space-y-3" onSubmit={handleSendMail}>
       <input
         type="text"
+        name="senderName"
         placeholder="Your name"
         required
         className="input w-full bg-color-5/50 shadow-none text-color-4"
       />
       <input
         type="text"
+        name="senderEmail"
         placeholder="Your email"
         required
         className="input w-full bg-color-5/50 shadow-none text-color-4"
@@ -18,6 +48,7 @@ const ContactForm = () => {
       <textarea
         className="textarea w-full bg-color-5/50 shadow-none text-color-4"
         placeholder="Your message"
+        name="message"
         required
       ></textarea>
       <button className="px-5 py-2 font-bold text-color-4 cursor-pointer rounded-sm flex items-center justify-center gap-2 bg-color-2 w-full">
