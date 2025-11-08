fe1f1fe822bd9df46b4e9337d29bbda4135d6763
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-11-08 19:25:50 +0600

    Merge branch 'fix-main-branch'

--- src/sections/education-section/EducationCard.tsx
+++ src/sections/education-section/EducationCard.tsx
@@ -17,11 +17,11 @@ function EducationCard({ education, length }: Props) {
         <div className={`flex items-stretch justify-center w-full gap-4 ${indx%2 && "lg:flex-row-reverse"}`}>
           {/* Left Card */}
           <div className="flex lg:hidden flex-col items-center">
-            <div className={`w-0.5 flex-1 border-2`} style={{borderColor: indx ? "#ffffffe6":"#072418"}} />
+            <div className={`w-0.5 flex-1 border-2`} style={{borderColor: indx ? "#ffffffe6":"#0a0a0a"}} />
             <div className="w-8 h-8 p-1 rounded-full border-2 border-primary flex items-center justify-center">
                 <GraduationCap size={24} />
             </div>
-            <div className={`w-0.5 flex-1 border-2`} style={{borderColor: indx === length-1 ? "#072418":"#ffffffe6"}} />
+            <div className={`w-0.5 flex-1 border-2`} style={{borderColor: indx === length-1 ? "#0a0a0a":"#ffffffe6"}} />
           </div> 
 
           <div className="flex-1 flex lg:justify-end py-2">
@@ -35,11 +35,11 @@ function EducationCard({ education, length }: Props) {
           {/* Center Timeline */}
           <div className="hidden lg:flex flex-col items-center">
             {/* <div className="w-[2px] flex-1 border-2 border-background" /> */}
-            <div className={`w-0.5 flex-1 border-2`} style={{borderColor: indx ? "#ffffffe6":"#072418"}} />
+            <div className={`w-0.5 flex-1 border-2`} style={{borderColor: indx ? "#ffffffe6":"#0a0a0a"}} />
             <div className="w-8 h-8 p-1 rounded-full border-2 border-primary flex items-center justify-center">
                 <GraduationCap size={24} />
             </div>
-            <div className={`w-0.5 flex-1 border-2`} style={{borderColor: indx === length-1 ? "#072418":"#ffffffe6"}} />
+            <div className={`w-0.5 flex-1 border-2`} style={{borderColor: indx === length-1 ? "#0a0a0a":"#ffffffe6"}} />
           </div> 
           <div className="flex-none lg:flex-1" />
         </div>
