ff595ff17b8cde8d2680d72bf6ed6c52d4a61b69
Sakibur Rahman <sakib.cse.333@gmail.com>
2026-04-27 21:01:54 +0600

    Add color stack.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
-    </div>
+    <>
+      <h1 className="text-2xl">Hello World!</h1>
+    </>
   )
 }
 
